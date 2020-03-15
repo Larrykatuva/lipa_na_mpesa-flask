59f6249ce4e83e960f144f022f18051cba8e8d2b
Biibadia <larry.katuva@gmail.com>
2020-03-15 19:57:04 +0300

    first commit

---
+++ keys.py
@@ -0,0 +1,5 @@
+business_shortCode =  "174379" # Bussiness short code
+phoneNumber = "254720460519" # phone number making the transaction
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key ="wT0AcFrWsS77p8teJ4DDGAD6OiEhy1GV"
+consumer_secret = "yqCAY0pvlvEqMhvA"
